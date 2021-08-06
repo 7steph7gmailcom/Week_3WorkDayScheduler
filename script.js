c48d144021dcb14847e058296c586166e5419f4d
stephaniecjohansson <7steph7@gmail.com>
2021-08-06 12:44:09 -0600

    3rdsave

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-var age = 45;
-console.log(age);
+// var age = 45;
+// console.log(age);
 
 var today = moment();
 console.log(today);
 
-var dayWeek = today.format("[Today is]dddd");
-$("currentDay").text(dayWeek);
+// var "currentDay" = today.format("[Today is]dddd");
+$("#currentDay").text(today.format("MMM Do YYYY"));
 
 // var currentDay = "weekDay";
 // var weekDay = moment("[Today is] dddd");

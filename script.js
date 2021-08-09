3f9f982914d8c6130206502e9466d9037646f68f
stephaniecjohansson <7steph7@gmail.com>
2021-08-08 18:47:00 -0600

    5thsave

--- script.js
+++ script.js
@@ -2,14 +2,4 @@
 // console.log(age);
 
 var today = moment();
-console.log(today);
-
-// var "currentDay" = today.format("[Today is]dddd");
-$("#currentDay").text(today.format("MMM Do YYYY"));
-
-// var currentDay = "weekDay";
-// var weekDay = moment("[Today is] dddd");
-
-// // 2. What is the day of the week today?
-// var dayWeek = today.format("[Today is] dddd")
-// $("#2a").text(dayWeek);
+$("#currentDay").text(today.format("MMM Do, YYYY"));

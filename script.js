477b1d1a7d9f8b71f9bd75a9a97c0ff5ed267dbc
stephaniecjohansson <7steph7@gmail.com>
2021-08-08 21:44:02 -0600

    6thsave

--- script.js
+++ script.js
@@ -3,3 +3,17 @@
 
 var today = moment();
 $("#currentDay").text(today.format("MMM Do, YYYY"));
+
+var inputToDoEl = $("ToDo");
+var btn btn-primary = saveButton;
+console.log("btn btn-primary");
+
+function inputList(event) {
+  event.preventDefault();
+
+  var calendarEvent = $(form - control).val();
+  console.log(calendarEvent);
+}
+
+inputEl.on("ToDo", handleFormSubmit);
+h;

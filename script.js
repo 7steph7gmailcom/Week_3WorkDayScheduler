2cadbce30c26df43bfa72f7d2dab714789afc1ef
stephaniecjohansson <7steph7@gmail.com>
2021-08-14 23:43:11 -0600

    saves

--- script.js
+++ script.js
@@ -3,20 +3,33 @@ var UserAssign = $("#taskAssignment");
 
 var saveButton = $(".saveBtn");
 
+var colorTimeBlocks = $(".time-blocks");
+console.log(colorTimeBlocks);
+
 // Display day of the week for user
 var today = moment();
 $("#currentDay").text(today.format("MMM Do, YYYY"));
 
-function saveTask() {
-  var userData = {
-    UserAssign: comment.value.trim(),
-  };
-  // setItem to get data to persist. JSON to translate data.
-  localStorage.setItem(UserAssign, JSON.stringify("taskAssignment"));
-  document.querySelector(UserAssign).value;
+// function saveTask() {
+//   var userData = {
+//     UserAssign: comment.value.trim(),
+//   };
+//   // setItem to get data to persist. JSON to translate data.
+//   localStorage.setItem(UserAssign, JSON.stringify("taskAssignment"));
+//   document.querySelector(UserAssign).value;
+// }
+
+setInterval(colorTimeBlocks, 60000);
+
+$(".time-block").each(function () {
+  var sectionId = $(this).attr("id");
+  $("#" + sectionId + " textarea").text(
+    localStorage.getItem(moment().format("DDDYYYY") + sectionId)
+  );
+});
+$("saveBtn").on("click", handleSave);
 }
 
-saveButton.on("click", function (event) {
-  event.preventDefault();
-  console.log("youve clicked the button");
-});
+// var saveUserInput = $(".col -md-10 description");
+// console.log(saveUserInput);
+
